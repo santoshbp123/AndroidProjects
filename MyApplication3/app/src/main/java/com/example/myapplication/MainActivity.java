506634d3041a781f9c1f774ed45180a9908e97d7
Santosh <santosh.android@gmail.com>
2025-01-13 17:37:41 +0530

    addedcode

--- MyApplication3/app/src/main/java/com/example/myapplication/MainActivity.java
+++ MyApplication3/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -14,5 +14,10 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main11);
         setContentView(R.layout.activity_main1);
         setContentView(R.layout.activity_main);
+        Class c = new class(0);
+                c.call()
+      san s = new san();
+                s.call();
+                santosh
     }
 }
