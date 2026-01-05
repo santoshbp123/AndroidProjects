b99872b2573e3111eca89a372e669d304d478595
santosh <santosh.android@gmail.com>
2026-01-05 11:57:33 +0530

    saving circle code

--- MyApplication3/app/src/main/java/com/example/myapplication/MainActivity.java
+++ MyApplication3/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -18,6 +18,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main31);
         setContentView(R.layout.activity_main);
         Class c = new class(0);
+        //changes to circle to rectanglw
                 c.call()
       san s = new san();
                 s.call();
